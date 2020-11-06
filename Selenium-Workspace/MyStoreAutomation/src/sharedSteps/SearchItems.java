9f47a6cfada6b13ade5758437d5bac950bcf2133
Aaron.Masters <aaron_masters_128@yahoo.com>
2020-11-06 15:29:59 +0000

    The Selenium Test now has the full logic worked out. It performs a search for each of the 7 items in the Search column, and for each search, it loops through 7 items in the list and determines whether or not they should appear in the search results. I have already worked out the xpath and other statements for performing the search confirmation, but have edited these out while I concentrate on getting the logic correct.

--- Selenium-Workspace/MyStoreAutomation/src/sharedSteps/SearchItems.java
+++ Selenium-Workspace/MyStoreAutomation/src/sharedSteps/SearchItems.java
@@ -15,8 +15,10 @@ public class SearchItems extends Browser
 		// Get locations of Search elements
 		WebElement searchField = driver.findElement(By.name("search_query"));
 		WebElement searchButton = driver.findElement(By.name("submit_search"));
-
-	
+		
+		// Delete the contents of the Search field before performing a new search
+		searchField.sendKeys("\u0001");
+		// Enter the Search criteria
 		searchField.sendKeys(searchItemName);
 
 		// Enter username and password then click Login or Submit Login
@@ -25,7 +27,9 @@ public class SearchItems extends Browser
 			.click()
 			.build();
 		SearchForItem.perform();
-	
+	}
+	public static void simpleCheckSearchResults()
+	{
 		// Confirm correct results appear based on price
 		WebElement results = driver.findElement(By.xpath("//*[@class='right-block']//*[@itemprop='price']"));
 		String resultPrice = results.getText();
@@ -33,6 +37,25 @@ public class SearchItems extends Browser
 		System.out.println("expectedPrice : "+expectedSearchResultPrice);
 		if(resultPrice.equals(expectedSearchResultPrice))
 		{
+			System.out.println("An item at the expected price was found");
+		}
+		else
+		{
+			System.out.println("The test did not find an item at the expected price");
+		}
+	}
+	
+	public static void advancedCheckSearchResults()
+	{
+		
+		// Confirm correct results appear based on price
+		WebElement results = driver.findElement(By.xpath("//*[@class='right-block']//*[@itemprop='price']"));
+		String resultPrice = results.getText();
+		System.out.println("resultPrice : "+resultPrice);
+		System.out.println("expectedPrice : "+expectedSearchResultPrice);
+		if(resultPrice.equals(expectedSearchResultPrice))
+		{
+			
 			System.out.println("An item at the expected price was found");
 		}
 		else
